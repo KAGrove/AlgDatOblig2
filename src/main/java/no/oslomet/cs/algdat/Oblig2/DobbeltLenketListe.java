0b951fbc324350f8359f1d6602fcb3a192301ad6
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-21 21:56:45 +0100

    Oppgave 1 - Passed

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -6,6 +6,7 @@ package no.oslomet.cs.algdat.Oblig2;
 
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Objects;
 
 
 public class DobbeltLenketListe<T> implements Liste<T> {
@@ -37,25 +38,42 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     private int endringer;         // antall endringer i listen
 
     public DobbeltLenketListe() {
-        throw new UnsupportedOperationException();
+        hode = hale = null;
+        antall = 0;
+        endringer = 0;
     }
 
+    // Oppgave 1
     public DobbeltLenketListe(T[] a) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(a, "Tabellen er tom");
+
+        new DobbeltLenketListe<>();
+        this.hode = new Node<>(null);
+        this.hale = hode;
+
+        for (int i = 0; i < a.length; i++){
+            if (a[i] != null){
+                hale.neste = new Node<>(a[i], hale, null);
+                hale = hale.neste;
+                antall++;
+            }
+        }
     }
 
     public Liste<T> subliste(int fra, int til) {
         throw new UnsupportedOperationException();
     }
 
+    // Oppgave 1
     @Override
     public int antall() {
-        throw new UnsupportedOperationException();
+        return antall;
     }
 
+    // Oppgave 1
     @Override
     public boolean tom() {
-        throw new UnsupportedOperationException();
+        return antall == 0;
     }
 
     @Override
